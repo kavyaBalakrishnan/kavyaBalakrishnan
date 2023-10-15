54ed54fee1e04d9298fc1d1e01a47bcf4c793744
kavyaBalakrishnan <kavya.b@live.com>
2023-10-15 01:08:56 -0400

    use query parameters instead of local storage

--- home.js
+++ home.js
@@ -1,24 +1,104 @@
-localStorage.clear();
-
 $(function(){
     $("#header").load("header.html");
 });
 
-$(function() {
-    $.getJSON('albums.json', function(data) {
-        $.each(data.albums, function(i, album) {
-            var element = document.createElement('div');
-            element.className = 'album-cover';
+$(function(){
+    const selectedAlbum = new URL(window.location.href).searchParams.get("album")
+    
+    if (selectedAlbum == null) {
+        // home page
+        document.getElementById("selectedAlbumView").style.display = "none"
+        
+        $.getJSON('albums.json', function(data) {
+            $.each(data.albums, function(i, album) {
+                var element = document.createElement('div');
+                element.className = 'album-cover';
+                
+                elementStyling = "<img src=\"" + album.coverImg + "\"/>" + "<div class=\"album-description\">" + "<h2>" + album.name + "</h2>" + "</div>";
+                element.innerHTML = elementStyling;
+                
+                $(element).appendTo("#albums");
+                
+                element.addEventListener("click", function() {
+                    const newQuery = new URLSearchParams({album: album.name})
+                    window.location.href = "/?" + newQuery
+                });
+            });
+        });
+    }
+    else {
+        // an album has been selected or user arrived directly to this url
+        $.getJSON('albums.json', function(data) {
+            const selectedAlbumData = data.albums.find(x => x.name == selectedAlbum)
+            if (selectedAlbumData == null) {
+                window.location.href = "/"
+            }
+            else {
+                document.getElementById("albums").style.display = "none"
+                
+                $("#selectedAlbumView").load("grid.html", function() {
+                    document.getElementById("subheader").innerHTML = "<h2>" + selectedAlbumData.name + "<\/h2>"
+                    populatePhotoGrid(selectedAlbumData.photos)
+                    setupMasonry()
+                    setupModals()
+                });
+            }
+        });
+    }
+});
 
-            elementStyling = "<a href=\""+ album.name +"\">" + "<img src=\"" + album.coverImg + "\"/>" + "<div class=\"album-description\">" + "<h2>" + album.name + "</h2>" + "</div>" + "</a>";
-            element.innerHTML = elementStyling;
+function populatePhotoGrid(albumPhotos){
+    $.each(albumPhotos, function(i, photo) {
+        var element = document.createElement('div');
+        element.className = 'grid-item';
+        
+        elementStyling = "<img src=\"" + photo.imgUrl + "\"/>"
+        element.innerHTML = elementStyling
+        
+        $(element).appendTo("#photo-grid")
+        
+        element.addEventListener("click", function() {
+            document.querySelector(".modalText").innerHTML = photo.description
+        });
+    });
+};
 
-            $(element).appendTo("#albums");
+function setupMasonry() {
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        gutter: '.gutter-sizer'
+    });
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry('layout');
+    });
+};
 
-            element.addEventListener("click", function() {
-                localStorage.setItem("currentAlbumSelection", album.name);
-                localStorage.setItem(album.name, JSON.stringify(album.photos));
+function setupModals() {
+    const images = document.querySelectorAll(".grid img");
+    const modal = document.querySelector(".modal");
+    const modalImg = document.querySelector(".modalImg");
+    const modalText = document.querySelector(".modalText");
+    const close = document.querySelector(".close");
+    
+    images.forEach((image) => {
+        image.addEventListener("click", () => {
+            modalImg.src = image.src
+            modal.classList.add("appear");
+            
+            modal.addEventListener("click", () => {
+                modal.classList.remove("appear");
+            });
+            
+            modalImg.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                return false;
+            });
+            
+            close.addEventListener("click", () => {
+                modal.classList.remove("appear");
             });
         });
     });
-});
+};
