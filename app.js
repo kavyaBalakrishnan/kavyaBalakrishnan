38d803228ddcd5b0efe7c06f339b152684d3538b
Balakrishnan, Kavya <kavya.b@live.com>
2024-09-27 20:53:53 +0100

    switched to a private access bucket

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const bucket = "kavya-photos"
-const bucketURL = `https://${encodeURIComponent(bucket)}.s3.amazonaws.com/`;
+const bucket = "kavya-pictures-originals"
 const apiVersion = "2006-03-01"
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
-    console.log(`Now listening on port ${port}`); 
+    console.log(`Now listening on port ${port}`);
 });
 
 AWS.config.region = process.env.AWS_REGION;
@@ -20,30 +19,42 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 });
 var s3 = new AWS.S3({
     apiVersion: apiVersion,
-    params: {Bucket: bucket},
+    params: { Bucket: bucket },
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname + "/public/views"});
+    res.sendFile('index.html', { root: __dirname + "/public/views" });
 });
 
 app.get('/gallery', (req, res) => {
-    res.sendFile('gallery.html', {root: __dirname + "/public/views"});
+    res.sendFile('gallery.html', { root: __dirname + "/public/views" });
 });
 
-app.get('/aws-config/bucket-URL', (req, res) => {
-    res.json(bucketURL)
-})
-
 app.get('/aws-config/album-list', (req, res) => {
-    s3.getObject({Key: "albums.json"}, function (err, data) {
+    s3.getObject({ Key: "albums.json" }, function (err, data) {
         res.json(data.Body.toString("utf-8"))
     })
 })
 
+app.get('/aws-config/photo-url', async (req, res) => {
+    var photoKey = decodeURIComponent(req.query.photoKey)
+    const params = {
+        Bucket: bucket,
+        Key: photoKey,
+        Expires: 60
+    };
+    try {
+        const url = await s3.getSignedUrlPromise('getObject', params);
+        res.status(200).json({ url });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+})
+
 app.get('/aws-config/album-photos', (req, res) => {
     var albumKey = decodeURIComponent(req.query.albumKey)
-    s3.listObjects({Prefix: albumKey}, function (err, data) {
+    s3.listObjects({ Prefix: albumKey }, function (err, data) {
         res.json(data.Contents.filter(file => file.Size > 0))
     })
 })
